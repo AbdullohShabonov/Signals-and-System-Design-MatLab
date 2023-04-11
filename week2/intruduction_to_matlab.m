840214f80bf963644e99f9186f266e4756820e72
Abdulloh Shabonov <abdullohshabonov03@gmail.com>
2023-04-11 13:22:25 +0900

    Introduction to retro language :)

---
+++ week2/intruduction_to_matlab.m
@@ -0,0 +1,18 @@
+% EX#1:     The calculation of the area and the circumference of the circle. 
+ 
+% assume that r = 4
+ r = 4
+ A = pi * r^2
+ C = 2 * pi * r
+  
+  
+  
+% EX#2:      Simple calculations in Matlab
+
+a = 4;b = 17.2;
+
+c = 5*a
+d = nthroot((b + 9.8) , 3)   % or  (b + 9.8)^(1/3)
+e = 10 * sqrt(5*a + 16)    % or   10 * (5*a + 16)^(1/2)
+  
+  
