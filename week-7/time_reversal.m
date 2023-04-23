97cc97c64ae1cef6e03143c0024aa4f760a1da08
Abdulloh Shabonov <abdullohshabonov03@gmail.com>
2023-04-23 14:45:04 +0900

    Create time_reversal.m

---
+++ week-7/time_reversal.m
@@ -0,0 +1,5 @@
+% In the operation of time reversing or for the general reversing through x-axis in any function we just need to aplly the reverse value of that axis
+% For example, y(x) = -2*(x+3)  the reversed function is, y(x) = -2*(-x+3)
+  
+  
+  
